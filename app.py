238f487de7f5d704de8cc2764b5bac71d9bda5b9
B1LL1E <karol660rata@gmail.com>
2026-03-02 22:13:32 +0100

    przepdubowane w 50% app.py

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
 from flask import Flask, render_template, request, redirect
-import sqlite3
-#from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
+import os
+import psycopg2
+from psycopg2.extras import RealDictConnection
 
 load_dotenv()
 
 app = Flask(__name__)
 
-#DB_PATH = 'seriale.db'
-#
-if os.getenv('DATABASE_URL'):
-    import psycopg2
-    from psycopg2.extras import RealDictConnection
-    DB_PATH = os.getenv('DATABASE_URL')
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'seriale.db')
+
+
+#LOKALNIE CZY ONLINE?
+if DATABASE_URL:
+    #laczy z baza PostgreSQL
+    def get_db_conn():
+        conn = psycopg2.connect(DATABASE_URL)
+        #wszystko ma byc na zywo zapisywane
+        conn.autocommit = True
+        return conn
     DB_TYPE = 'postgres'
 else:
+    #tylko lokalnie
+    import sqlite3
     DB_PATH = 'seriale.db'
     DB_TYPE = 'sqlite'
 
+#glowne polaczenie
 @app.route('/')
 def index():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM seriale')
+    if DB_TYPE == 'postgres':
+        conn = get_db_conn()
+        cur = conn.cursor(cursor_factory=RealDictConnection)
+    else:
+        conn = sqlite3.connect(DATABASE_URL)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    cur.execute('SELECT * FROM seriale ORDER BY id DESC')
     seriale = cur.fetchall()
     conn.close()
     return render_template('index.html', seriale = seriale)
@@ -39,31 +53,46 @@ def add_serial():
     autor_avt = request.form['autor_avt']
     zrodlo = request.form['zrodlo']
 
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-    cur.execute('INSERT INTO seriale (tytul, nr_odc_poczatek, nr_odc_koniec, opis, avt, jezyk, autor_avt, zrodlo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s,)', (tytul, nr_odc_poczatek, nr_odc_koniec, opis, avt, jezyk, autor_avt, zrodlo))
-    conn.commit()
-    conn.close()
+    if DB_TYPE == 'postgres':
+        conn = get_db_conn()
+        cur = conn.cursor()
+        cur.execute('''
+            INSERT INTO seriale (tytul, nr_odc_poczatek, nr_odc_koniec, opis, avt, jezyk, autor_avt, zrodlo) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        ''', (tytul, nr_odc_poczatek, nr_odc_koniec, opis, avt, jezyk, autor_avt, zrodlo))
+        conn.close()
+    else:
+        conn = sqlite3.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute('''
+            INSERT INTO seriale (tytul, nr_odc_poczatek, nr_odc_koniec, opis, avt, jezyk, autor_avt, zrodlo) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (tytul, nr_odc_poczatek, nr_odc_koniec, opis, avt, jezyk, autor_avt, zrodlo))
+        conn.commit()
+        conn.close()
     return redirect('/')
 
-if __name__ == '__main__':
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
-    cur.execute('''
-                CREATE TABLE IF NOT EXISTS seriale(
-                    id SERIAL PRIMARY KEY,
-                    tytul TEXT,
-                    nr_odc_poczatek INTEGER,
-                    nr_odc_koniec INTEGER,
-                    opis TEXT,
-                    avt TEXT NOT NULL CHECK(avt IN ('dubbing', 'lektor', 'napisy', 'inne')),
-                    jezyk TEXT NOT NULL CHECK(jezyk IN ('polski', 'english', 'inne')),
-                    autor_avt TEXT,
-                    zrodlo TEXT NOT NULL
-                );
-                ''')
-    conn.commit()
-    conn.close()
-    app.run(debug=True)
 
+if __name__ == '__main__':
+    # TYLKO lokalnie SQLite
+    if DB_TYPE == 'sqlite':
+        conn = sqlite3.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS seriale(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                tytul TEXT,
+                nr_odc_poczatek INTEGER,
+                nr_odc_koniec INTEGER,
+                opis TEXT,
+                avt TEXT NOT NULL,
+                jezyk TEXT NOT NULL,
+                autor_avt TEXT,
+                zrodlo TEXT NOT NULL
+            );
+        ''')
+        conn.commit()
+        conn.close()
+    
+    app.run(debug=True)
 
